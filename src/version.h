bf7dbfcb419f37baeed1c035d98e1044b410eacd
CryptoCoderz <CryptoCoderz@gmail.com>
2018-01-05 08:59:47 -0800

    Espers v0.8.7.1 Update-1 Patch-1
    Updated connection parameters

--- src/version.h
+++ src/version.h
@@ -30,13 +30,13 @@ static const int DATABASE_VERSION = 70509;
 //
 // network protocol versioning
 //
-static const int PROTOCOL_VERSION = 60031;
+static const int PROTOCOL_VERSION = 60032;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60030;
+static const int MIN_PEER_PROTO_VERSION = 60031;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
