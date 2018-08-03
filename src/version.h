2114d6159a2e96d157de47dcacb14e43535e88f5
CryptoCoderz <CryptoCoderz@gmail.com>
2018-08-02 18:48:40 -0700

    Espers v0.8.7.3 Update-0 Patch-4
    Changelog
    
    - adjusted protocol versioning to allow for proper peer connection post initial sync.

--- src/version.h
+++ src/version.h
@@ -36,7 +36,7 @@ static const int PROTOCOL_VERSION = 60034;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60033; // jumped for testing
+static const int MIN_PEER_PROTO_VERSION = 60033;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -44,10 +44,10 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 0;
-static const int NOBLKS_VERSION_END = 60033; // jumped for testing
+static const int NOBLKS_VERSION_END = 60032;
 
 // hard cutoff time for legacy network connections
-static const int64_t HRD_LEGACY_CUTOFF = 1515153600; // ON (TOGGLED Jan/05/2018)
+static const int64_t HRD_LEGACY_CUTOFF = 1536451200; // ON (TOGGLED Sep/09/2018)
 
 // hard cutoff time for future network connections
 static const int64_t HRD_FUTURE_CUTOFF = 9993058800; // OFF (NOT TOGGLED)
