61c560185559eaff1ce94da7f8858b642116680c
CryptoCoderz <CryptoCoderz@gmail.com>
2018-12-04 18:01:53 -0800

    Espers v0.8.7.5 Patch-4 Xnode-2
    Changelog
    
    - Version bump and sync fix for testing on mainnet

--- src/version.cpp
+++ src/version.cpp
@@ -36,12 +36,12 @@ const std::string CLIENT_NAME("Espers");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "Patch 4"
-#    define GIT_COMMIT_DATE "Dec 3, 2018" //$Format:%cD
+#    define GIT_COMMIT_ID "Patch-4 Xnode-2"
+#    define GIT_COMMIT_DATE "Dec 4, 2018" //$Format:%cD
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-" commit
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) " " commit
 
 #define BUILD_DESC_FROM_UNKNOWN(maj,min,rev,build) \
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-"
