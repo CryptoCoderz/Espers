9bc1836c0ad289e68ff41213e4ef4b17ac0dad3c
CryptoCoderz <CryptoCoderz@gmail.com>
2024-07-12 07:27:10 -0700

    Update boost_placeholders.h

--- src/primitives/boost_placeholders.h
+++ src/primitives/boost_placeholders.h
@@ -11,15 +11,15 @@ namespace boost
 {
     namespace placeholders
     {
-        inline boost::arg<1> _1() { return boost::arg<1>(); }
-        inline boost::arg<2> _2() { return boost::arg<2>(); }
-        inline boost::arg<3> _3() { return boost::arg<3>(); }
-        inline boost::arg<4> _4() { return boost::arg<4>(); }
-        inline boost::arg<5> _5() { return boost::arg<5>(); }
-        inline boost::arg<6> _6() { return boost::arg<6>(); }
-        inline boost::arg<7> _7() { return boost::arg<7>(); }
-        inline boost::arg<8> _8() { return boost::arg<8>(); }
-        inline boost::arg<9> _9() { return boost::arg<9>(); }
+        static inline boost::arg<1> _1() { return boost::arg<1>(); }
+        static inline boost::arg<2> _2() { return boost::arg<2>(); }
+        static inline boost::arg<3> _3() { return boost::arg<3>(); }
+        static inline boost::arg<4> _4() { return boost::arg<4>(); }
+        static inline boost::arg<5> _5() { return boost::arg<5>(); }
+        static inline boost::arg<6> _6() { return boost::arg<6>(); }
+        static inline boost::arg<7> _7() { return boost::arg<7>(); }
+        static inline boost::arg<8> _8() { return boost::arg<8>(); }
+        static inline boost::arg<9> _9() { return boost::arg<9>(); }
     } // namespace placeholders
 } // namespace boost
 
